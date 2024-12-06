752c4886cd9f7f8b47fdfcb20a1589566a3ef9b8
AgusDharmaUDEL93 <agusdharmaudel@gmail.com>
2024-12-06 18:59:50 +0700

    Initial Commit

---
+++ Spotify-Assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotify-Assignment
+//
+//  Created by Putu Agus Dharma Kusuma on 06/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotify-Assignment/Spotify_AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_AssignmentApp.swift
+//  Spotify-Assignment
+//
+//  Created by Putu Agus Dharma Kusuma on 06/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
